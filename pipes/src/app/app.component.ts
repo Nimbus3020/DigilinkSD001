7f7360a4be6560f5c65ad8d2264f21bdb74eb315
Nimbus3020 <kthuthukani4@gmail.com>
2023-05-05 09:28:33 +0200

    Mastering Pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string | undefined;
+  date: string | undefined;
+  amount: number | undefined;
+  height: number | undefined;
+  miles: number | undefined;
+
+
+ car = {
+  make: 'Toyota',
+  model: 'Camry',
+  year: 2000
+ };
+
+  onMilesChange(value: string)
+  {this.miles=parseFloat(value);}
+  
+  onHeightChange(value: string)
+  {this.height = parseFloat(value);}
+
+  onNameChange(value: string)
+  {this.name = value;}
+
+  onDateChange(value: string)
+  {this.date = value;}
+
+  onAmountChange(value: string)
+  {this.amount = parseFloat(value);}
+}
